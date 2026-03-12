6480e216bc6b401623a4bf9225e0da144758964f
Rplus <rplus.tw@gmail.com>
2026-03-13 00:18:46 +0800

    fix: change corsproxy service

--- src/component/Share.svelte
+++ src/component/Share.svelte
@@ -18,9 +18,12 @@
 
 		is_fetching = true;
 
-		const encoded_url = encodeURIComponent(`http://tinyurl.com/api-create.php?url=${url}`);
+		// const encoded_url = encodeURIComponent(`http://tinyurl.com/api-create.php?url=${url}`);
 
-		short_href = await fetch_data(`https://corsproxy.io/?${encoded_url}`, 'text');
+		// short_href = await fetch_data(`https://corsproxy.io/?${encoded_url}`, 'text');
+		// short_href = await fetch_data(`https://api.allorigins.win/raw?url=${encoded_url}`, 'text');
+		short_href = await fetch_data(`https://cors-anywhere.herokuapp.com/http://tinyurl.com/api-create.php?url=${url}`, 'text');
+		console.log(2221, short_href);
 		// TODO: preview shorturl https://tinyurl.com/preview/ooxxoxox
 		is_fetching = null;
 	}
@@ -41,26 +44,46 @@
 				{$_('share.full.url')}
 			</a>
 		</li>
-
-		<li>
-			<a class="short-link text-decoration:"
-				href={short_href}
-				class:fetching={is_fetching}
-				rel="noopener"
-				target="_blank"
-			>
-				{$_('share.short.link')}
-			</a>
-
-			{#if short_href}
-				<img class="display:block margin-top:2px" src={ get_qrcode_img(short_href) } alt="qrcode for short url">
-			{/if}
-		</li>
 	</ul>
-	<div class="text-align:right">
-		<hr>
-		<button onclick={gen_short_href}>📦 {$_('share.get.short.url')}</button>
-	</div>
+
+	<details>
+		<summary>If you need to get a short link</summary>
+		<div>
+			<div>
+				1. Go to cors-anywhere site,
+				<br>
+				and click its button to request temporary access.
+				<br>
+				<a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">https://cors-anywhere.herokuapp.com/corsdemo</a>
+			</div>
+
+			<div>
+				2. Click
+				<br>
+				<button onclick={gen_short_href}>📦 {$_('share.get.short.url')}</button>
+			</div>
+
+			<div>
+				3.
+				Here is
+				<a class="short-link text-decoration:"
+					href={short_href}
+					class:fetching={is_fetching}
+					rel="noopener"
+					target="_blank"
+				>
+					{$_('share.short.link')}
+				</a>
+			</div>
+
+			<div>
+				{#if short_href}
+					<img class="display:block margin-top:2px" src={ get_qrcode_img(short_href) } alt="qrcode for short url">
+				{/if}
+			</div>
+		</div>
+
+	</details>
 </div>
 
 

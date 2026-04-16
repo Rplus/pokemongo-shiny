73e45d1cfb2270c7324b292ef7cecfc5434fd93d
Rplus <rplus.tw@gmail.com>
2026-04-16 10:21:59 +0800

    task: add defailt dev port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import dsv from '@rollup/plugin-dsv';
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/pokemongo-shiny/',
+	server: {
+		port: 7258,
+	},
 	build: {
 		target: 'esnext',
 	},
